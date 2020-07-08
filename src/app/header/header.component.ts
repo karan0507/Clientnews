dfa94a1b5fd3b10dc188d0f610f30451e131154f
Yash <yash.talegaonkar37@gmail.com>
2020-07-08 22:43:42 +0530

    changes in nav and single new component
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,31 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  show = false;
   categoriesList = [
-    {name: ' टॉप न्यूज़' , path: '/top-news'},
+    {name: 'टॉप न्यूज़' , path: '/top-news'},
     {name: 'आंतरराष्ट्रीय', path: '/international-news'},
     {name: 'राष्ट्रीय', path: '/national-news'},
     {name: 'राज्य', path: '/state-news'},
     {name: 'गंमत जंमत', path: '/funny-news'},
     {name: 'फिल्मीबास्केट', path: '/filimy-basket-news'},
     {name: 'हेल्थ गुरु', path: '/health-guru-news'},
+   
+   
+  ];
+  categoriesListMore = [
+   
     {name: 'क्राईम स्टोरीज', path: '/crime-stories-news'},
     {name: 'टेक नो लवर', path: '/techno-lover-news'},
     {name: 'फनिशनी', path: '/funny-shunny-news'},
     {name: 'कृषिजीवन', path: '/farmers-news'}
-   
-  ];
-  categoriesListMore = [{
-    either:  [{ name: ' टॉप न्यूज़' , path: '/top-news'}]
-  }
+  
   ];
   constructor() { }
   title = 'गर्जा महाराष्ट्र';
   ngOnInit(): void {
-    console.log(this.categoriesList);
+    // console.log(this.categoriesListMore[0].either[0].name);
 
     
   }
 
+  onhover() {
+    this.show = true;
+  }
+
+  onleave() {
+    // setInterval(() => {
+    //   this.show = false;
+    // }, 1000);
+    this.show = false;
+  }
 }
