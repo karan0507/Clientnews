58e10e58ed64b38e744b77486d5e2cb885031fd4
Yash <yash.talegaonkar37@gmail.com>
2020-07-15 03:40:44 +0530

    header and info page
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

--- src/app/single-news/single-news.component.ts
+++ src/app/single-news/single-news.component.ts
@@ -12,16 +12,23 @@ export class SingleNewsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private newsdb: NewsService, private title: Title ) { }
   selectedPost;
-  tags: Array<any>;
+  tags;
   tagsValue;
+  
   ngOnInit(): void {
     this.title.setTitle(this.route.snapshot.data.NewsResolve.headline);
     this.getPostById(this.route.snapshot.data.NewsResolve.id);
     this.tags = this.route.snapshot.data.NewsResolve.tags_id;
-    console.log(this.tags);
-    this.tags.forEach(function(value)  {
-      this.getTagsValueById(value);
+    console.log(JSON.parse(this.tags));
+    // this.tags.push('4');
+    let test =  JSON.parse(this.tags);
+    test.forEach( function(val) {
+      console.log(val);
+      this.getTagsValueById(val);
     });
+    // JSON.parse(this.tags.forEach(function(value)  {
+    //   this.getTagsValueById(value);
+    // });
     }
 
    async getPostById(id) {
@@ -32,11 +39,12 @@ export class SingleNewsComponent implements OnInit {
     }
 
     getTagsValueById(tags) {
-     this.newsdb.getTagsById(tags).subscribe(res => {
+      console.log('hh');
+      this.newsdb.getTagsById(tags).subscribe(res => {
        console.log(res);
        this.tagsValue = res;
      });
 
-    } 
+    }
 
 }
