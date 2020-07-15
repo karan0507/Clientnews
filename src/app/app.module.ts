4f897beb5688cb6c02979b8b869e40a2acf0b841
Yash <yash.talegaonkar37@gmail.com>
2020-07-15 14:54:57 +0530

    universal enabled
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 // import { NgtUniversalModule } from '@ng-toolkit/universal';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 // import { TransferHttpCacheModule } from '@nguniversal/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Inject, PLATFORM_ID, APP_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +39,7 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports:[
     CommonModule,
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
     HttpClientModule,
     NgxGoogleAnalyticsModule.forRoot('UA-150950243-1'),
     AppRoutingModule,
@@ -47,4 +48,12 @@ import { FooterComponent } from './footer/footer.component';
   ],
   providers: [HttpClient, CategoriesPipe, TruncatePipe, NewsResolve],
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,24 +1,17 @@
-import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
-import {NgModule} from '@angular/core';
-    import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
-    import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
-    import { BrowserModule } from '@angular/platform-browser';
-    import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-    
-    @NgModule({
- bootstrap: [AppComponent],
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
 
-        imports:[
- BrowserModule.withServerTransition({appId: 'app-root'}),
- 
- AppModule,
- 
-            ServerModule,
-            NoopAnimationsModule,
-            ModuleMapLoaderModule,
-            ServerTransferStateModule, // comment
-        ]
-    })
-    export class AppServerModule {}
-    
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    AppModule,
+    ServerModule,
+  ],
+  providers: [
+    // Add server-only providers here.
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppServerModule {}
